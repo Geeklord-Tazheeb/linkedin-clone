6b6eb2fb0bc45fa880874b021117dbecdf11050d
Geeklord-Tazheeb <thshamsi@hotmail.com>
2022-03-28 15:20:12 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import './App.css';
-import Feed from './components/Feed';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Feed from "./components/Feed";
+import { auth } from "./components/firebase";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import {login, logout,selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    auth.onAuthStateChanged(userAuth =>{
+      if (userAuth){
+        //user logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName:userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }));
+      } else {
+        //user not logged 
+        dispatch(logout());
+      }
+    })
+  },[])
+
   return (
-    <div className="App">
-   
-        {/* Header */}
-        <Header/>
-    
-        {/* App body */}
-      <div className="app__body">
-        
-        {/* Sidebar */}
-        <Sidebar/>
-
-        {/* Feeds */}
-        <Feed/>
-
-        {/* Widgets */}
-
+    <div className="App">      
+      <Header />
+      
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
         </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import {useDispatch} from "react-redux"
+import {login} from "../features/userSlice"
+import {auth} from "./firebase"
+import "./Login.css"
+
+function Login() {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [name,setName] = useState("");
+    const [profilePicture,setProfilePicture] = useState("");
+    const dispatch= useDispatch();
+
+
+const loginToApp = (e) => {
+    e.preventDefault();
+};
+
+const register = () =>{
+    if (!name) {
+        return alert("Please enter your full name");
+    }
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((userAuth) => {
+        userAuth.user.updateProfile({
+        displayName: name,
+        photoUrl: profilePicture,
+        })
+        .then(() => { 
+            dispatch(login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName:name,
+                photoUrl: profilePicture
+            }))
+        })
+    }).catch((error) => alert(error));
+};
+
+  return (
+    <div className= "login">
+        <img
+         src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+          alt="bannerLogo" />
+
+        <form>
+            <input value={name} onChange={(e) =>setName(e.target.value)} type="text" placeholder=" Full Name (required)" />
+            <input value={email} onChange={(e) =>setEmail(e.target.value)} type="email" placeholder=" Your e-mail (required)" />
+            <input value={password} onChange={(e) =>setPassword(e.target.value)} type="text" placeholder="Profile pic URL (optional)" />            
+            <input value={profilePicture} onChange={(e) =>setProfilePicture(e.target.value)} type="text" placeholder="Enter your password" />
+            <button type="submit" onClick={loginToApp}>Sign In</button>
+        </form>
+        <p>New to LinkedIn? {""} <span class="login__register" onClick={register}>Join Now</span></p>
+    </div>
+  )
+}
+
+export default Login
